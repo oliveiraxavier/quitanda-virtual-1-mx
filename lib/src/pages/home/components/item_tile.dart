ed7fb6a8caf8c36a9cc9626ac89a041b309a10a4
Marcos Xavier <oliveiraxavi@gmail.com>
2022-10-13 22:40:59 -0300

    Start quitanda virtual - Aula 28

--- lib/src/pages/home/components/item_tile.dart
+++ lib/src/pages/home/components/item_tile.dart
@@ -3,38 +3,50 @@ import 'package:quitanda_virtual_mx/src/config/custom_colors.dart';
 import 'package:quitanda_virtual_mx/src/models/item_model.dart';
 import 'package:quitanda_virtual_mx/src/services/utils_services.dart';
 
+import '../../product/product_screen.dart';
+
 class ItemTile extends StatelessWidget {
   final ItemModel item;
 
   ItemTile({Key? key, required this.item}) : super(key: key);
 
   UtilsServices utilsServices = UtilsServices();
+
   @override
   Widget build(BuildContext context) {
     return Stack(
       children: [
-        Card(
-          elevation: 3,
-          shadowColor: Colors.grey.shade300,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(15),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.all(12.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                //Imagem
-                Expanded(
-                  child: Image.asset(item.imgUrl),
-                ),
-                //Nome
-                Text(item.itemName,
-                    style:
-                        const TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
-                //Preco - Unidade
-                Row(
-                  children: [
+        //Conteudo
+        GestureDetector(
+          onTap: () {
+            Navigator.of(context).push(MaterialPageRoute(
+              builder: (c) => ProductScreen(
+                item: item,
+              ),
+            ));
+          },
+          child: Card(
+            elevation: 3,
+            shadowColor: Colors.grey.shade300,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(15),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  //Imagem
+                  Expanded(
+                    child:
+                        Hero(tag: item.imgUrl, child: Image.asset(item.imgUrl)),
+                  ),
+                  //Nome
+                  Text(item.itemName,
+                      style: const TextStyle(
+                          fontSize: 16, fontWeight: FontWeight.bold)),
+                  //Preco - Unidade
+                  Row(children: [
                     Text(
                       utilsServices.priceToCurrency(item.price),
                       style: TextStyle(
@@ -51,9 +63,9 @@ class ItemTile extends StatelessWidget {
                         color: Colors.grey.shade500,
                       ),
                     ),
-                  ]
-                )
-              ],
+                  ])
+                ],
+              ),
             ),
           ),
         ),
@@ -63,24 +75,19 @@ class ItemTile extends StatelessWidget {
           top: 4,
           right: 4,
           child: GestureDetector(
-            onTap: () {
-
-            },
+            onTap: () {},
             child: Container(
               height: 40,
               width: 35,
               decoration: BoxDecoration(
                 color: CustomColors.customSwatchColor,
                 borderRadius: const BorderRadius.only(
-                    bottomLeft: Radius.circular(15),
-                    topRight: Radius.circular(15),
+                  bottomLeft: Radius.circular(15),
+                  topRight: Radius.circular(15),
                 ),
               ),
-              child: const Icon(
-                Icons.add_shopping_cart_outlined,
-                color: Colors.white,
-                size: 20
-              ),
+              child: const Icon(Icons.add_shopping_cart_outlined,
+                  color: Colors.white, size: 20),
             ),
           ),
         ),
---
+++ lib/src/pages/product/product_screen.dart
@@ -0,0 +1,132 @@
+import 'package:flutter/material.dart';
+import 'package:quitanda_virtual_mx/src/config/custom_colors.dart';
+import 'package:quitanda_virtual_mx/src/models/item_model.dart';
+import 'package:quitanda_virtual_mx/src/services/utils_services.dart';
+
+class ProductScreen extends StatelessWidget {
+  ProductScreen({Key? key, required this.item}) : super(key: key);
+
+  final ItemModel item;
+  final UtilsServices utilsServices = UtilsServices();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white.withAlpha(230),
+      body: Stack(
+        //Conteúdo
+        children: [
+          Column(
+            children: [
+              //Imagem
+              Expanded(
+                child: Hero(tag: item.imgUrl, child: Image.asset(item.imgUrl)),
+              ),
+
+              //Card
+              Expanded(
+                child: Container(
+                    padding: const EdgeInsets.all(32),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius:
+                          const BorderRadius.vertical(top: Radius.circular(35)),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.shade600,
+                          offset: const Offset(0, 2),
+                        ),
+                      ],
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        //Nome - Quantidade
+                        Row(
+                          children: [
+                            Expanded(
+                              child: Text(
+                                item.itemName,
+                                maxLines: 2,
+                                overflow: TextOverflow.ellipsis,
+                                style: const TextStyle(fontSize: 27),
+                              ),
+                            ),
+                            Container(
+                              height: 30,
+                              width: 70,
+                              color: Colors.red,
+                            )
+                          ],
+                        ),
+
+                        // Preço
+                        Text(
+                          utilsServices.priceToCurrency(item.price),
+                          style: TextStyle(
+                            fontSize: 23,
+                            fontWeight: FontWeight.bold,
+                            color: CustomColors.customSwatchColor,
+                          ),
+                        ),
+
+                        //Descrição
+                        Expanded(
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 100),
+                            child: SingleChildScrollView(
+                              child: Text(
+                                item.description * 25,
+                                style: const TextStyle(
+                                  height: 1.5,
+                                  // fontSize: 23,
+                                  // fontWeight: FontWeight.bold,
+                                  // color: CustomColors.customSwatchColor,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+
+                        //Botão
+                        SizedBox(
+                          height: 50,
+                          child: ElevatedButton.icon(
+                            onPressed: () {},
+                            label: Text(
+                              'Adicionar ao carrinho',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 18,
+                              ),
+                            ),
+                            style: ElevatedButton.styleFrom(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(15),
+                              ),
+                            ),
+                            icon: const Icon(Icons.shopping_cart_outlined),
+                          ),
+                        ),
+                      ],
+                    )),
+              ),
+            ],
+          ),
+
+          //Botão voltar
+          Positioned(
+            top: 10,
+            left: 10,
+            child: SafeArea(
+              child: IconButton(
+                onPressed: () => Navigator.of(context).pop(),
+                icon: const Icon(Icons.arrow_back_ios),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
