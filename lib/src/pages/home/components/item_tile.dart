03efb48a24e0ed8ae245956c6bd0d2bc0b24b23a
Marcos Xavier <oliveiraxavi@gmail.com>
2022-10-24 22:00:09 -0300

    Start quitanda virtual - Aula 49

--- lib/src/pages/home/components/item_tile.dart
+++ lib/src/pages/home/components/item_tile.dart
@@ -2,13 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:quitanda_virtual_mx/src/config/custom_colors.dart';
 import 'package:quitanda_virtual_mx/src/models/item_model.dart';
 import 'package:quitanda_virtual_mx/src/services/utils_services.dart';
-
 import '../../product/product_screen.dart';
 
 class ItemTile extends StatelessWidget {
   final ItemModel item;
+  final void Function(GlobalKey) cartAnimationMethod;
+  final GlobalKey imageGk = GlobalKey();
 
-  ItemTile({Key? key, required this.item}) : super(key: key);
+  ItemTile({
+      Key? key,
+      required this.item,
+      required this.cartAnimationMethod
+  }) : super(key: key);
 
   final UtilsServices utilsServices = UtilsServices();
 
@@ -38,26 +43,33 @@ class ItemTile extends StatelessWidget {
                 children: [
                   //Imagem
                   Expanded(
-                    child:
-                        Hero(tag: item.imgUrl, child: Image.asset(item.imgUrl)),
+                    child: Hero(
+                        tag: item.imgUrl,
+                        child: Container(
+                            key: imageGk,
+                            child: Image.asset(item.imgUrl)
+                        )
+                    ),
                   ),
                   //Nome
-                  Text(item.itemName,
-                      style: const TextStyle(
-                          fontSize: 16, fontWeight: FontWeight.bold)),
+                  Text(
+                    item.itemName,
+                    style: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
                   //Preco - Unidade
-                  Row(
-                    children: [
-                      Text(
-                          utilsServices.priceToCurrency(item.price),
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                            color: CustomColors.customSwatchColor,
-                          ),
-                        ),
-
-                      Text(
+                  Row(children: [
+                    Text(
+                      utilsServices.priceToCurrency(item.price),
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: CustomColors.customSwatchColor,
+                      ),
+                    ),
+                    Text(
                       '/${item.unit}',
                       style: TextStyle(
                         fontWeight: FontWeight.bold,
@@ -65,8 +77,7 @@ class ItemTile extends StatelessWidget {
                         color: Colors.grey.shade500,
                       ),
                     ),
-                    ]
-                  ),
+                  ]),
                 ],
               ),
             ),
@@ -78,7 +89,9 @@ class ItemTile extends StatelessWidget {
           top: 4,
           right: 4,
           child: GestureDetector(
-            onTap: () {},
+            onTap: () {
+              cartAnimationMethod(imageGk);
+            },
             child: Container(
               height: 40,
               width: 35,
@@ -89,8 +102,11 @@ class ItemTile extends StatelessWidget {
                   topRight: Radius.circular(15),
                 ),
               ),
-              child: const Icon(Icons.add_shopping_cart_outlined,
-                  color: Colors.white, size: 20),
+              child: const Icon(
+                Icons.add_shopping_cart_outlined,
+                color: Colors.white,
+                size: 20,
+              ),
             ),
           ),
         ),
--- lib/src/pages/home/home_tab.dart
+++ lib/src/pages/home/home_tab.dart
@@ -1,3 +1,5 @@
+import 'package:add_to_cart_animation/add_to_cart_animation.dart';
+import 'package:add_to_cart_animation/add_to_cart_icon.dart';
 import 'package:badges/badges.dart';
 import 'package:flutter/material.dart';
 import '../../config/app_data.dart' as app_data;
@@ -6,7 +8,6 @@ import 'components/item_tile.dart';
 import 'components/category_tile.dart';
 
 class HomeTab extends StatefulWidget {
-
   const HomeTab({super.key});
 
   @override
@@ -14,14 +15,19 @@ class HomeTab extends StatefulWidget {
 }
 
 class _HomeTabState extends State<HomeTab> {
-
   String selectedCategory = 'Frutas';
+  GlobalKey<CartIconKey> globalKeyCartItems = GlobalKey<CartIconKey>();
+  late Function(GlobalKey gkImage) runAddToCardAnimation;
+
+  void itemSelectedCartAnimations(GlobalKey gkImage) {
+    runAddToCardAnimation(gkImage);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       // App bar
-      appBar: AppBar  (
+      appBar: AppBar(
         centerTitle: true,
         elevation: 0,
         backgroundColor: Colors.transparent,
@@ -42,6 +48,7 @@ class _HomeTabState extends State<HomeTab> {
           ]),
         ),
         actions: [
+
           //Icone carrinho topo
           Padding(
             padding: const EdgeInsets.only(
@@ -49,9 +56,7 @@ class _HomeTabState extends State<HomeTab> {
               right: 30,
             ),
             child: GestureDetector(
-              onTap: () {
-
-              },
+              onTap: () {},
               child: Badge(
                 badgeColor: CustomColors.customContrastColor,
                 badgeContent: const Text(
@@ -61,93 +66,107 @@ class _HomeTabState extends State<HomeTab> {
                     fontSize: 10,
                   ),
                 ),
-                child: Icon(
-                  Icons.shopping_cart,
-                  color: CustomColors.customSwatchColor,
-                ),
-              ),
-            ),
-          ),
-        ],
-      ),
-
-      body: Column(
-        children: [
-          //Campo de pesquisa
-          Padding(
-            padding: const EdgeInsets.symmetric(
-              horizontal: 20,
-              vertical: 10,
-            ),
-            child: TextFormField(
-              decoration: InputDecoration(
-                filled: true,
-                fillColor: Colors.white,
-                isDense: true,
-                hintText: 'Pesquise aqui...',
-                prefixIcon: Icon(
-                  Icons.search,
-                  color: CustomColors.customContrastColor,
-                  size: 30,
-                ),
-                hintStyle: TextStyle(color: Colors.grey.shade400, fontSize: 14),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(60),
-                  borderSide: const BorderSide(
-                    width: 0,
-                    style: BorderStyle.none,
+                child: AddToCartIcon(
+                  key: globalKeyCartItems,
+                  icon: Icon(
+                    Icons.shopping_cart,
+                    color: CustomColors.customSwatchColor,
                   ),
                 ),
               ),
             ),
           ),
+        ],
+      ),
 
-          // Categorias
-          Container(
-            padding: const EdgeInsets.only(left: 6),
-            height: 40,
-            child: ListView.separated(
-              scrollDirection: Axis.horizontal,
-              itemBuilder: (_, int index) {
-                return CategoryTile(
+      body: AddToCartAnimation(
+        //Animação Cart
+        gkCart: globalKeyCartItems,
+        previewDuration: const Duration(milliseconds: 100),
+        previewCurve: Curves.easeInOutCirc,
+        receiveCreateAddToCardAnimationMethod: (addToCardAnimationMethod) {
+          runAddToCardAnimation = addToCardAnimationMethod;
+        },
+        child: Column(
+          children: [
+            //Campo de pesquisa
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 20,
+                vertical: 10,
+              ),
+              child: TextFormField(
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.white,
+                  isDense: true,
+                  hintText: 'Pesquise aqui...',
+                  prefixIcon: Icon(
+                    Icons.search,
+                    color: CustomColors.customContrastColor,
+                    size: 30,
+                  ),
+                  hintStyle:
+                      TextStyle(color: Colors.grey.shade400, fontSize: 14),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(60),
+                    borderSide: const BorderSide(
+                      width: 0,
+                      style: BorderStyle.none,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+
+            // Categorias
+            Container(
+              padding: const EdgeInsets.only(left: 6),
+              height: 40,
+              child: ListView.separated(
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (_, int index) {
+                  return CategoryTile(
                     onPressed: () {
-                      setState((){
+                      setState(() {
                         selectedCategory = app_data.categories[index];
                       });
-
                     },
                     category: app_data.categories[index],
                     isSelected: app_data.categories[index] == selectedCategory,
-                );
-              },
-              separatorBuilder: (_, int index) => const SizedBox(width: 10,),
-              itemCount: app_data.categories.length,
+                  );
+                },
+                separatorBuilder: (_, int index) => const SizedBox(
+                  width: 10,
+                ),
+                itemCount: app_data.categories.length,
+              ),
             ),
-          ),
-          // Categorias
+            // Categorias
 
-          // Grid  Produtos
-
-          Expanded(
-            child: GridView.builder(
-              padding: const EdgeInsets.fromLTRB(16, 0, 16,16),
-              physics: const BouncingScrollPhysics(),
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            // Grid  Produtos
+            Expanded(
+              child: GridView.builder(
+                padding: const EdgeInsets.fromLTRB(16, 0, 16, 16),
+                physics: const BouncingScrollPhysics(),
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
                   crossAxisCount: 2,
                   mainAxisSpacing: 10,
                   crossAxisSpacing: 10,
                   childAspectRatio: 9 / 11.5,
+                ),
+                itemCount: app_data.items.length,
+                itemBuilder: (_, index) {
+                  return ItemTile(
+                    item: app_data.items[index],
+                    cartAnimationMethod: itemSelectedCartAnimations
+                  );
+                },
               ),
-              itemCount: app_data.items.length,
-              itemBuilder: (_, index) {
-                return ItemTile(item: app_data.items[index]);
-              },
             ),
-          ),
-        ],
+          ],
+        ),
       ),
-
-
     );
   }
 }
